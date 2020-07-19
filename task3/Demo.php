e4d88e5d29204d6a2a1200a7d4253687b9259290
KrystianGrabowski <krystian987543@gmail.com>
2020-07-19 11:17:50 +0200

    Add task3 demo

--- task3/Demo.php
+++ task3/Demo.php
@@ -5,13 +5,45 @@ require 'vendor/autoload.php';
 use Money\Money;
 use src\Bundle;
 use src\Product;
-//use src\Discounted;
+use src\Discounted;
 
 try 
 {
-    $bundle0 = new Bundle("FirstBundle", new Product(Money::USD(500), "headphones"),
-                                   new Product(Money::USD(650), "headphones"));
-    echo $bundle0->getPrice()->getAmount();
+    $bundle0 = new Bundle("FirstBundle", new Product(Money::USD(500), "product1"),
+                                         new Product(Money::USD(650), "product2"),
+                                         new Product(Money::USD(122), "product3"),
+                                         new Product(Money::USD(123), "product4"));
+    $bundle1 = new Bundle("SecondBundle", new Product(Money::USD(250), "product1"),
+                                          new Product(Money::USD(250), "product2"),
+                                          new Product(Money::USD(250), "product3"),
+                                          new Product(Money::USD(250), "product4"));
+    
+    $discounted0 = new Discounted(10, new Product(Money::USD(1000), "product1"));
+    $discounted1 = new Discounted(20, $bundle1);
+    
+    $product1   = new Product(Money::USD(10000), "product1");
+    $product2   = new Product(Money::USD(10000), "product2");
+    $totalPrice = Money::USD(0);
+    
+    
+    $products = [
+        $product1,
+        $product2,
+        $bundle0,
+        $bundle1,
+        $discounted0,
+        $discounted1
+    ];
+    
+    foreach ($products as $product)
+    {
+        echo $product->getName() . PHP_EOL;
+        echo $product->getPrice()->getAmount() . PHP_EOL;
+    
+        $totalPrice = $totalPrice->add($product->getPrice());
+    }
+    
+    echo 'TOTAL PRICE: ' . $totalPrice->getAmount() . PHP_EOL;
 } 
 catch (Exception $e) 
 {
---
+++ task3/src/Discounted.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace src;
+
+use Exception;
+use Money\Money;
+
+class Discounted implements IProduct
+{
+    private $procent;
+    private $product;
+
+    function __construct(int $procent, IProduct $product)
+    {
+        if ($procent < 1 || $procent > 100) 
+        {
+            return new Exception("Discount procent should be between 1 and 100");
+        }
+        $this->procent = $procent;
+        $this->product = $product;
+    }
+
+    function getName(): string
+    {
+        return $this->product->getName();
+    }
+    function getPrice(): Money
+    {
+        $price = $this->product->getPrice();
+        $subVal = $price->multiply($this->procent / 100);
+        return $price->subtract($subVal); 
+    }
+}
+
+?>
